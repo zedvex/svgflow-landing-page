76e6c63186c0634e11310ad98b1970a68e785ec4
Ian-Wanjira <ianwanjira4@gmail.com>
2024-11-27 07:00:49 -0600

    feat: add dark mode and extend Tailwind CSS configuration
    - Enabled dark mode with the  strategy.
    - Included content paths for the  directory.
    - Added container centering with responsive padding.
    - Set Inter font as the default sans-serif font.
    - Extended screen sizes with custom breakpoints.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: ['class'],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './sections/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          md: '2rem',
+          lg: '4rem',
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)', 'sans-serif'],
+      },
+      screens: {
+        sm: '375px',
+        md: '768px',
+        lg: '1200px',
       },
     },
   },
