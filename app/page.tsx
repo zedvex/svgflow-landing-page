28b0837ed3c149c93fbec5a27358a8cc79ca55e1
zedvex <zedvex@gmail.com>
2025-01-21 21:56:33 +1300

    fix: fix for vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Features from '@/sections/Features';
 import Footer from '@/sections/Footer';
 import Hero from '@/sections/Hero';
 // import Integrations from '@/sections/Integrations';
-import Introduction from '@/sections/Introduction';
+// import Introduction from '@/sections/Introduction';
 // import LogoTicker from '@/sections/LogoTicker';
 
 const Home = () => {
